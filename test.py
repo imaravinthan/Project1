40695922adee5293bb5dfb10539ec5c4a8e7eb20
imaravinthan <aravinthan.selvaraj@icloud.com>
2023-11-24 22:24:37 +0530

    New entry

--- test.py
+++ test.py
@@ -1,1 +1,13 @@
-print("Welcome")
+import codecs
+import os
+
+readme = "README.md"
+direc = os.listdir('./')
+if os.path.exists(os.path.join(os.getcwd(),readme)):
+    print("File is available")
+    file_lines = open(readme,errors="ignore")
+
+    for line in file_lines:
+        print(line)
+else:
+    print("File is not available....")
